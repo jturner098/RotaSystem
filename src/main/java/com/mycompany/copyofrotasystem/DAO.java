de7ec83c869866e0784df0dfbbb6b94d887584b0
josephturner <josephturner@localhost>
2023-09-29 11:00:00 +0100

    28/9/23 - Began work on DAO class

---
+++ src/main/java/com/mycompany/copyofrotasystem/DAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.copyofrotasystem;
+
+/**
+ *
+ * @author josephturner
+ */
+public class DAO {
+    private static final String CONN_URL = "jdbc:mysql://computing.gfmat.org:3306/";
+    private static final String DB_NAME = "RotaSystem";
+    private static final String USER = "2022d_JTurner";
+    private static final String PASSWORD = "JBmz8nwJe63FmUcq";
+    
+    
+}
