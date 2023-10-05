2e59add9e322a097309ee8264bedfee28ab1967f
josephturner <josephturner@localhost>
2023-10-05 10:06:32 +0100

    5/10/23 - Fixed JTable problems, started work on Create New User screen

--- src/main/java/com/mycompany/copyofrotasystem/DAO.java
+++ src/main/java/com/mycompany/copyofrotasystem/DAO.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.copyofrotasystem;
+import java.sql.*;
 
 /**
  *
@@ -14,5 +15,27 @@ public class DAO {
     private static final String USER = "2022d_JTurner";
     private static final String PASSWORD = "JBmz8nwJe63FmUcq";
     
+    public static ResultSet GetUserDetails() throws SQLException {
+        String sql = "SELECT * FROM tblStaff";
+
+        Connection con = DriverManager.getConnection(CONN_URL + DB_NAME, USER, PASSWORD);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        return rs;
+     
+    }
     
-}
+    public static ResultSet UserShifts() throws SQLException{
+        String sql = "SELECT locationid, shiftdate, starttime, endtime FROM tblshift";
+        
+        Connection con = DriverManager.getConnection(CONN_URL + DB_NAME, USER, PASSWORD);
+        System.out.println("Connection Made");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        return rs;
+        } 
+    }
+
+    
+
+
